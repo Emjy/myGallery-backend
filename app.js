bba2cc327410833a40a6962844f62cea8f1d46f5
Emilien <emiliengir@gmail.com>
2024-04-01 13:06:34 +0200

    change route destroy

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
+// Charger les variables d'environnement en premier
 require('dotenv').config();
 
+// Importations des modules nécessaires
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
+// Configuration de la connexion à la base de données
 const mongoose = require('mongoose');
 require('./models/connection');
 
-// Autoriser des domaines spécifiques
-const corsOptions = {
-    origin: ['https://art-papa-frontend.vercel.app', 'http://localhost:3001'],
-    methods: ['GET', 'POST', 'DELETE'] // Autoriser seulement les requêtes DELETE
-
-};
-
-
+// Importation des routeurs
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var afficheRouter = require('./routes/affiches');
 var tableauxRouter = require('./routes/tableaux');
 var photosRouter = require('./routes/photos');
 
-
+// Initialisation de l'application express
 var app = express();
 
-const cors = require('cors');
+// Configuration de CORS pour autoriser des domaines spécifiques
+const corsOptions = {
+    origin: ['https://art-papa-frontend.vercel.app', 'http://localhost:3001'],
+};
 app.use(cors(corsOptions));
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+// Configuration des middlewares
+app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuration des routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/affiches', afficheRouter)
-app.use('/tableaux', tableauxRouter)
-app.use('/photos', photosRouter)
+app.use('/affiches', afficheRouter);
+app.use('/tableaux', tableauxRouter);
+app.use('/photos', photosRouter);
 
+// Lancement du serveur
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 module.exports = app;
