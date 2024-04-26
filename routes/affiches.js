277dd127c7c5c1a688dbaae950f6e5f2028e5408
Emilien <emiliengir@gmail.com>
2024-04-26 16:13:51 +0200

    API IMGBB

--- routes/affiches.js
+++ routes/affiches.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+const axios = require('axios');
+
 
 const multer = require('multer');
 const upload = multer({ dest: '/tmp/uploads' });
