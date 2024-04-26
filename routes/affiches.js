a4251fffb329c9b1dcfa656b438e56046a911c9c
Emilien <emiliengir@gmail.com>
2024-04-26 16:12:17 +0200

    retour version cloudinary

--- routes/affiches.js
+++ routes/affiches.js
@@ -35,28 +35,56 @@ router.get("/", async (req, res) => {
 
 // Post an affiche
 router.post("/", upload.single('file'), async (req, res) => {
-
   try {
+    // URL de l'endpoint ImgBB pour télécharger une image
+    const uploadUrl = "https://api.imgbb.com/1/upload";
 
-    const resultCloudinary = await cloudinary.uploader.upload(req.file.path, {
-      folder: "Affiches",
+    // Remplacez 'YOUR_API_KEY' par votre clé API ImgBB
+    const apiKey = '08031bcaa1a14c2050baadf36c10d07d';
+
+    // Chemin vers l'image que vous souhaitez télécharger
+    const imagePath = req.file.path;
+
+    // Lecture de l'image en tant que flux binaire
+    const imageStream = fs.createReadStream(imagePath);
+
+    // Création du payload de la requête POST
+    const formData = new FormData();
+    formData.append('key', apiKey);
+    formData.append('image', imageStream);
+
+    // Envoi de la requête POST à ImgBB pour télécharger l'image
+    const response = await axios.post(uploadUrl, formData, {
+      headers: formData.getHeaders()
     });
 
-    const newAffiche = new Affiche({
-      imageName: resultCloudinary.secure_url,
-      idCloud: resultCloudinary.public_id,
-      filmName: req.body.filmName,
-      realName: req.body.realName,
-      creationDate: new Date()
-    });
+    // Vérification de la réponse et récupération de l'URL de l'image téléchargée
+    if (response.status === 200 && response.data.success) {
+      const imageUrl = response.data.data.url;
 
-    const affiche = await newAffiche.save();
-    await unlinkAsync(req.file.path); // Assurez-vous d'effacer le fichier temporaire
-    res.json({ result: true, affiche });
+    // Création d'une nouvelle Affiche avec les données de l'image téléchargée depuis ImgBB
+      const newAffiche = new Affiche({
+        imageName: imageUrl,
+        // idCloud: response.data.public_id, // ImgBB ne fournit pas de public_id
+        filmName: req.body.filmName,
+        realName: req.body.realName,
+        creationDate: new Date()
+      });
 
+      // Sauvegarde de la nouvelle Affiche dans la base de données
+      const affiche = await newAffiche.save();
+
+      // Suppression du fichier temporaire une fois qu'il a été téléchargé et enregistré
+      await unlinkAsync(imagePath);
+
+      // Réponse JSON avec le résultat et les données de l'affiche sauvegardée
+      res.json({ result: true, affiche });
+    } else {
+      throw new Error('Failed to upload image to ImgBB');
+    }
   } catch (error) {
     console.error('An error occurred:', error);
-    await unlinkAsync(req.file.path); // En cas d'erreur, effacez aussi le fichier
+    await unlinkAsync(req.file.path); // Suppression du fichier temporaire en cas d'erreur
     res.status(500).json({ result: false, error: error.message });
   }
 });
