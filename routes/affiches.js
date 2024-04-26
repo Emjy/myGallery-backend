83275a5d8459d919d431efea060c6f7ca67c1bbb
Emilien <emiliengir@gmail.com>
2024-04-26 14:03:58 +0200

    ajout gestion airtable

--- routes/affiches.js
+++ routes/affiches.js
@@ -51,7 +51,7 @@ router.post("/", upload.single('file'), async (req, res) => {
         {
           fields: {
             Name: req.body.filmName,
-            Image: [{ url: req.file.path }]
+            Image: [{ url: base64Image }]
 
           }
         }
