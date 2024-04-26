5af9cceafb270207ab0329ea62f2b2d414a05064
Emilien <emiliengir@gmail.com>
2024-04-26 15:47:03 +0200

    test api imgix

--- routes/affiches.js
+++ routes/affiches.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 const axios = require('axios');
-const fs = require('fs');
 
 const multer = require('multer');
 const upload = multer({ dest: '/tmp/uploads' });
